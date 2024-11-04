8cc08956f455183c716630fdfece738fd97d90e9
Minh Nguyen <mtnguyen.dev@gmail.com>
2024-11-04 16:44:28 -0700

    added initial Spring initializer files

---
+++ backend/src/test/java/mn/leetfreak/LeetFreakApplicationTests.java
@@ -0,0 +1,13 @@
+package mn.leetfreak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeetFreakApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
