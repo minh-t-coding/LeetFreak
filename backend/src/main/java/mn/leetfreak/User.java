7eb9ed2c81c370138e30548c3c29865b6b64d5aa
Minh Nguyen <mtnguyen.dev@gmail.com>
2024-11-20 01:01:35 -0700

    added Entity, Controller, Repository, and Service classes for Users

---
+++ backend/src/main/java/mn/leetfreak/User.java
@@ -0,0 +1,60 @@
+package mn.leetfreak;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY) // Auto-increments id
+    private Long id;
+
+    private String username;
+    private String email;
+    private String passwordHash;
+    private int eloRating;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPasswordHash() {
+        return passwordHash;
+    }
+
+    public void setPasswordHash(String passwordHash) {
+        this.passwordHash = passwordHash;
+    }
+
+    public int getEloRating() {
+        return eloRating;
+    }
+
+    public void setEloRating(int eloRating) {
+        this.eloRating = eloRating;
+    }
+}
